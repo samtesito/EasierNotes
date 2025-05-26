b3e953731bd57cbcec20ebf492ffdf3e52a098be
Matias Leon <mrleon.24@est.ucab.edu.ve>
2025-05-26 13:42:30 -0400

    [+] Clases js para enrutamiento y carga de vistas

---
+++ src/Frontend/Scripts/noteList.js
@@ -0,0 +1,9 @@
+
+window.NoteList = {
+    render: function() {
+        let html = `<p> Logica para cargar html de notas </p>`;
+
+        $('#main-frame').html(html);
+
+    }
+}
---
+++ src/Frontend/Scripts/noteParticular.js
@@ -0,0 +1,9 @@
+
+window.NoteParticular = {
+    render: function() {
+        let html = `<p> Logica para cargar html de notas </p>`;
+
+        $('#main-frame').html(html);
+
+    }
+}
