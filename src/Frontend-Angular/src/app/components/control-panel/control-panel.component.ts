3412295c06065e38fdda420f208fd6d77bee52e5
Matias Leon <mrleon.24@est.ucab.edu.ve>
2025-05-26 23:38:16 -0400

    [+] Listado de notas arreglado

--- src/Frontend-Angular/src/app/components/control-panel/control-panel.component.ts
+++ src/Frontend-Angular/src/app/components/control-panel/control-panel.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-control-panel',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/Frontend-Angular/src/app/components/note-list/note-list.component.ts
+++ src/Frontend-Angular/src/app/components/note-list/note-list.component.ts
@@ -1,17 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { NotesService } from '../../services/notes.service';
 import { Note } from '../../models/Note';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note-list',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NoteListComponent {
   notesService = inject(NotesService);  
+  router = inject(Router);
   notes = this.notesService.MockNotes;
   selectedNote = signal<Note | null>(null);
 
@@ -22,24 +23,33 @@ export class NoteListComponent {
 
     return  contentOverview.length > 100 ? contentOverview.substring(0, 100) + '...' : contentOverview
   };
+
   unselectNote(){
     this.selectedNote.set(null);
   }
+
   selectNote(selectedNote: Note){
     if (selectedNote.Id === this.selectedNote()?.Id){
-      this.unselectNote; 
+      this.unselectNote(); 
     }else{
       this.selectedNote.set(selectedNote);
+      console.log("selectedNote", this.selectedNote());
     }
   }
-  alternateSelection(currentNote: Note): boolean {
+
+  isSelected(currentNote: Note): boolean {
+    let result = false;
+    console.log("isNull", this.selectedNote()===null);
     if (currentNote.Id===this.selectedNote()?.Id){ 
-      return false;
+      result =  true;
     } 
-    return true
+    console.log("isSelected", result, "currentNote.Id", currentNote.Id);
+    return result;
   }
-  
-    
+
+  openNote(noteId: number) {
+    this.router.navigate(['/note', noteId]);
 }
 
+}
 
