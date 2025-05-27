deb572a2020e3ab779270ef95ac4f7406a3f736e
Matias Leon <mrleon.24@est.ucab.edu.ve>
2025-05-27 03:02:32 -0400

    cambios

--- src/Backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ src/Backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851ce31064f77b4b72dfeb12ceee7f152a9b0c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94fc354f631541920943003bdf2e360832edd37")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
